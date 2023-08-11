1a5d2bf29b6deb91396b2352a95582f4da6d8b64
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-08-12 00:52:25 +0900

    Rendering - Matrix ~ 2.2 scale

---
+++ Rendering/Assets/1.Matrices/Scripts/PositionTransformation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PositionTransformation : Transformation 
+{
+    public Vector3 position;
+
+    public override Vector3 Apply (Vector3 point) 
+    {
+        return point + position;
+    }
+}
